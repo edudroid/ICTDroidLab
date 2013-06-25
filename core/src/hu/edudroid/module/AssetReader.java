d88d1c45983396d42ce8336dfd184fb57fa4a8b7
wpatrik14 <wpatrik14@gmail.com>
2013-06-25 14:21:02 +0200

    assets copy

---
+++ core/src/hu/edudroid/module/AssetReader.java
@@ -0,0 +1,34 @@
+package hu.edudroid.module;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.Context;
+import android.util.Log;
+
+public class AssetReader {
+	private static final String TAG = "AssetReader";
+	
+	public static File copyAssetToInternalStorage(String assetPath, Context context) {
+		Log.d(TAG, "Copying asset " + assetPath);
+    	File outFile = new File(context.getFilesDir(),assetPath);
+		Log.d(TAG, "Output file: " + outFile.getAbsolutePath());
+	    try {
+	    	FileOutputStream outStream = context.openFileOutput(assetPath, Context.MODE_PRIVATE);	    	
+	        InputStream in = context.getAssets().open(assetPath);
+	        int read;
+	        byte[] buffer = new byte[4096];
+	        while ((read = in.read(buffer)) > 0) {
+	        	outStream.write(buffer, 0, read);
+	        }
+	        outStream.close();
+	        in.close();
+	    } catch (IOException e) {
+	    	e.printStackTrace();
+	       Log.e(TAG, "Couldn't copy asset " + assetPath + " to internal file " + assetPath + " " + e.getMessage());
+	    }
+        return outFile;
+	}
+}
