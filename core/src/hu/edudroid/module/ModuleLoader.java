4844b2bf7a7f4b47098799d32bd29681bd6e263c
wpatrik14 <wpatrik14@gmail.com>
2013-06-26 09:49:32 +0200

    ModuleLoader

--- core/src/hu/edudroid/module/ModuleLoader.java
+++ core/src/hu/edudroid/module/ModuleLoader.java
@@ -1,15 +1,9 @@
 package hu.edudroid.module;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+
 import android.content.Context;
+import android.util.Log;
 import dalvik.system.DexClassLoader;
 
 public class ModuleLoader {
@@ -31,16 +25,38 @@ public class ModuleLoader {
 
 	@SuppressWarnings("rawtypes")
 	private ModuleRunnable loadModule(File file){
+		
 		final File optimizedDexOutputPath = mContext.getDir("outdex",
-															Context.MODE_PRIVATE);
+															
+
+Context.MODE_PRIVATE);
+		
 		DexClassLoader cl = new DexClassLoader(	file.getAbsolutePath(),
-												optimizedDexOutputPath.getAbsolutePath(),
+												
+
+optimizedDexOutputPath.getAbsolutePath(),
 												null,
-												mContext.getClassLoader());
+												
+
+mContext.getClassLoader());
+		
 		Class moduleRunnerClass = null;
 		try{
-			moduleRunnerClass = cl.loadClass("hu.tmit.ictdroid.module.ModuleRunnable");
+			/*
+			URL url = file.toURL();  
+			URL[] urls = new URL[]{url};
+			ClassLoader cl = new URLClassLoader(urls);
+			
+			moduleRunnerClass = cl.loadClass("hu.edudroid.ict.sample_project.ModuleExample");
 			ModuleRunnable module = (ModuleRunnable) moduleRunnerClass.newInstance();
+			*/
+			
+			moduleRunnerClass = cl.loadClass("hu.edudroid.ict.sample_project.ModuleExample");
+			ModuleRunnable module = (ModuleRunnable) moduleRunnerClass.newInstance();
+			
+			
+			Log.e("Modul","Modul has been loaded succesfully");
+			
 			return module;
 		}
 		catch (Exception exception){
@@ -51,6 +67,7 @@ public class ModuleLoader {
 	
 	public void runModule(String urlString, String fileUrl){
 		try {
+			/*
 			URL url = new URL(urlString);
 			URLConnection connection = url.openConnection();
 			connection.connect();
@@ -67,16 +84,15 @@ public class ModuleLoader {
 			output.flush();
 			output.close();
 			input.close();
-			ModuleRunnable module = loadModule(new File(fileUrl));
+			*/
+			File outFile = new File(this.mContext.getFilesDir(),fileUrl);
+			ModuleRunnable module = loadModule(new File(outFile.getAbsolutePath()));
 			module.run();
+			
+			Log.e("ModuleLoader","Modul is running");
+			
 		} catch (NullPointerException e) {
 			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 }
