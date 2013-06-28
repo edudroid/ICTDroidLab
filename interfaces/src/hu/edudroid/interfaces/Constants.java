84acd8e2f865e6b02b425f5962e151c171bfbf8c
Lajtha András Balázs <lajthabalazs@yahoo.com>
2013-06-28 14:26:57 +0200

    Added constants.

---
+++ interfaces/src/hu/edudroid/interfaces/Constants.java
@@ -0,0 +1,20 @@
+package hu.edudroid.interfaces;
+
+public class Constants {
+
+	public static final String INTENT_EXTRA_CALL_ID = "id";
+	public static final String INTENT_EXTRA_METHOD_NAME = "methodname";
+	public static final String INTENT_EXTRA_METHOD_PARAMETERS = "methodparams";
+	public static final String INTENT_ACTION_CALL_METHOD = "hu.edudroid.ict.plugin_polling_answer";
+	public static final String INTENT_ACTION_DESCRIBE = "hu.edudroid.ict.plugin_polling_answer";
+	public static final String INTENT_ACTION_REQUEST_DESCRIPTION = "hu.edudroid.ict.plugin_polling_answer";
+	public static final String INTENT_ACTION_RESULT = "hu.edudroid.ict.plugin_polling_answer";
+	
+	public static final String INTENT_EXTRA_KEY_PLUGIN_ID = "plugin";
+	public static final String INTENT_EXTRA_KEY_PLUGIN_AUTHOR = "author";
+	public static final String INTENT_EXTRA_KEY_DESCRIPTION = "description";
+	public static final String INTENT_EXTRA_KEY_VERSION = "version";
+	public static final String INTENT_EXTRA_KEY_PLUGIN_METHODS = "methods";
+	public static final String INTENT_EXTRA_VALUE_RESULT = "reoprt result";
+	public static final String INTENT_EXTRA_VALUE_ERROR = "report error";
+}
