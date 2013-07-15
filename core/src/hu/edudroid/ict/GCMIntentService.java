36f1dd96257a65cea993e1095939cebac65a41f4
wpatrik14 <wpatrik14@gmail.com>
2013-07-15 12:56:52 +0200

    Google Cloud Messaging

---
+++ core/src/hu/edudroid/ict/GCMIntentService.java
@@ -0,0 +1,88 @@
+package hu.edudroid.ict;
+
+import hu.edudroid.ict.ui.MainActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
+ 
+public class GCMIntentService extends GCMBaseIntentService {
+ 
+    private static final String TAG = "GCMIntentService";
+ 
+    public GCMIntentService() {
+        super(MainActivity.SENDER_ID);
+    }
+ 
+    /**
+     * Method called on device registered
+     **/
+    @Override
+    protected void onRegistered(Context context, String registrationId) {
+        Log.i(TAG, "Device registered: regId = " + registrationId);
+        //displayMessage(context, "Your device registred with GCM");
+        //ServerUtilities.register(context, "ICTDroidLab", "wpatrik14@gmail.com", registrationId);
+    }
+ 
+    /**
+     * Method called on device un registred
+     * */
+    @Override
+    protected void onUnregistered(Context context, String registrationId) {
+        Log.i(TAG, "Device unregistered");
+        //displayMessage(context, getString(R.string.gcm_unregistered));
+        //ServerUtilities.unregister(context, registrationId);
+    }
+ 
+    /**
+     * Method called on Receiving a new message
+     * */
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+        Log.i(TAG, "Received message");
+        String message = intent.getExtras().getString("message");
+         
+        //displayMessage(context, message);
+        // notifies user
+        generateNotification(context, message);
+    }
+ 
+    /**
+     * Method called on receiving a deleted message
+     * */
+    @Override
+    protected void onDeletedMessages(Context context, int total) {
+        Log.i(TAG, "Received deleted messages notification");
+        String message = getString(R.string.gcm_deleted, total);
+        //displayMessage(context, message);
+        // notifies user
+        generateNotification(context, message);
+    }
+ 
+    /**
+     * Method called on Error
+     * */
+    @Override
+    public void onError(Context context, String errorId) {
+        Log.i(TAG, "Received error: " + errorId);
+        //displayMessage(context, getString(R.string.gcm_error, errorId));
+    }
+ 
+    @Override
+    protected boolean onRecoverableError(Context context, String errorId) {
+        // log message
+        Log.i(TAG, "Received recoverable error: " + errorId);
+        //displayMessage(context, getString(R.string.gcm_recoverable_error,errorId));
+        return super.onRecoverableError(context, errorId);
+    }
+ 
+    /**
+     * Issues a notification to inform the user that server has sent a message.
+     */
+    private static void generateNotification(Context context, String message) {
+        Log.e("MESSAGE RECEIVED",message);
+        
+    }
+ 
+}
