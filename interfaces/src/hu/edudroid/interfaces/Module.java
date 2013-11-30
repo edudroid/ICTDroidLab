abe75694b37a214f980a660765f62ea1dbfbb7ac
kissgabor <nanashiy@gmail.com>
2013-11-30 17:00:50 +0100

    Extended Module interface with ThreadSemaphore

--- interfaces/src/hu/edudroid/interfaces/Module.java
+++ interfaces/src/hu/edudroid/interfaces/Module.java
@@ -6,13 +6,15 @@ public abstract class Module implements PluginResultListener, PluginEventListene
 	protected final Logger 					mLogger;
 	protected final PluginCollection 			mPluginCollection;
 	protected final TimeServiceInterface		mTimeService;
+	protected final ThreadSemaphore			mThreadSemaphore;
 	
 	
-	public Module(Preferences prefs, Logger logger, PluginCollection pluginCollection, TimeServiceInterface timeservice) {
+	public Module(Preferences prefs, Logger logger, PluginCollection pluginCollection, TimeServiceInterface timeservice, ThreadSemaphore threadsemaphore) {
 		mPrefs = prefs;
 		mLogger = logger;
 		mPluginCollection = pluginCollection;
 		mTimeService = timeservice;
+		mThreadSemaphore = threadsemaphore;
 	}
 	
 	public abstract void init();
