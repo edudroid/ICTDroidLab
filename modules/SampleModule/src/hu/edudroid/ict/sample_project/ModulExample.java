d996a45ed04a4aedad346aa3f7435d224c619812
kissgabor <nanashiy@gmail.com>
2013-11-30 17:16:30 +0100

    Extended ModulExample with ThreadSemaphore and other methods

--- modules/SampleModule/src/hu/edudroid/ict/sample_project/ModulExample.java
+++ modules/SampleModule/src/hu/edudroid/ict/sample_project/ModulExample.java
@@ -1,49 +1,48 @@
 package hu.edudroid.ict.sample_project;
-
 import hu.edudroid.interfaces.Logger;
 import hu.edudroid.interfaces.Module;
 import hu.edudroid.interfaces.PluginCollection;
 import hu.edudroid.interfaces.Preferences;
 import hu.edudroid.interfaces.TimeServiceInterface;
+import hu.edudroid.interfaces.ThreadSemaphore;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-
 public class ModulExample extends Module {
-	
-	SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS");
-	
-	public ModulExample(Preferences prefs, Logger logger, PluginCollection pluginCollection, TimeServiceInterface timeservice) {
-		super(prefs, logger, pluginCollection, timeservice);
+  SimpleDateFormat dateFormatter=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS");
+  public ModulExample(  Preferences prefs,  Logger logger,  PluginCollection pluginCollection,  TimeServiceInterface timeservice,  ThreadSemaphore threadsemaphore){
+    super(prefs,logger,pluginCollection,timeservice,threadsemaphore);
+  }
+  private static final String TAG="ModuleExample";
+  @Override public void init(){
+    mLogger.e(TAG,"Module init...");
+    mTimeService.runPeriodic(1000,5000,0,this);
+  }
+  @Override public void onResult(  long id,  String plugin,  String pluginVersion,  String methodName,  List<String> result){
+  }
+  @Override public void onError(  long id,  String plugin,  String pluginVersion,  String methodName,  String errorMessage){
+  }
+  @Override public void onEvent(  String plugin,  String version,  String eventName,  List<String> extras){
+  }
+  @Override public void onTimerEvent(){
+    setTid();
+    threadSleeper();
+    mLogger.d(TAG,"timer event");
+    mLogger.i(TAG,"Module example run at " + dateFormatter.format(new Date()));
+  }
+  public void setTid(){
+    mThreadSemaphore.setThreadId();
+  }
+  public void threadSleeper(){
+    if (mThreadSemaphore.availablePermits() == 0) {
+      long time=500;
+      try {
+		Thread.sleep(time);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-	
-	private static final String TAG = "ModuleExample";
-	
-	@Override
-	public void init(){
-		mLogger.e(TAG, "Module init...");
-		mTimeService.runPeriodic(1000, 5000, 0, this);
-	}
-	
-	@Override
-	public void onResult(long id, String plugin, String pluginVersion,
-			String methodName, List<String> result) {
-	}
-
-
-	@Override
-	public void onError(long id, String plugin, String pluginVersion, String methodName,
-			String errorMessage) {
-	}
-
-	@Override
-	public void onEvent(String plugin, String version, String eventName, List<String> extras) {
-	}
-
-	@Override
-	public void onTimerEvent() {
-		mLogger.d(TAG, "timer event");
-		mLogger.i(TAG, "Module example run at " + dateFormatter.format(new Date()));
-	}
-} 
+    }
+  }
+}
