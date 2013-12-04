10eaf96066281f34554d090b682c4e97c206ebe8
kissgabor <nanashiy@gmail.com>
2013-12-04 15:03:39 +0100

    Added individual limit and frofiling mod to settings

--- core/src/hu/edudroid/ict/CoreService.java
+++ core/src/hu/edudroid/ict/CoreService.java
@@ -138,10 +138,12 @@ public class CoreService extends Service implements PluginListener {
 				e.printStackTrace();
 			}
 
-			List<ModuleDescriptor> moduleDescriptors = ModuleLoader.getAllModules(this);
+			List<ModuleDescriptor> moduleDescriptors = ModuleLoader
+					.getAllModules(this);
 			for (ModuleDescriptor moduleDescriptor : moduleDescriptors) {
 				if (moduleDescriptor.getState(this) == ModuleState.INSTALLED) {
-					moduleManager.startModule(moduleDescriptor, pluginCollection);
+					moduleManager.startModule(moduleDescriptor,
+							pluginCollection);
 				}
 			}
 
@@ -153,8 +155,10 @@ public class CoreService extends Service implements PluginListener {
 				@Override
 				public void run() {
 					float avgrunning_time;
+					float sumavgrunning_time;
 					while (true) {
 						try {
+							sumavgrunning_time = 0;
 							cpulimit = getCpuUserSettings();
 							list = moduleManager.getLoadedModuleSemaphores();
 							for (int i = 0; i < list.size(); i++) {
@@ -162,27 +166,63 @@ public class CoreService extends Service implements PluginListener {
 								if (thrs.getThreadId() != 0) {
 									running_time = totalRunTime(processid,
 											thrs.getThreadId());
-		
-									if(thrs.sizeofList() == 15){
+									Log.d("cpucontrol", Integer.toString(thrs
+											.getThreadId()));
+									if (thrs.sizeofList() == 15) {
 										thrs.removefromList();
 									}
 									thrs.addtoList(running_time);
 									avgrunning_time = 0;
 									int denominator = 1;
-					
-									if(thrs.sizeofList() == 1){
-										avgrunning_time = thrs.getObjectfromList(0);
-									} else{
-										for(int j = 1; j != thrs.sizeofList(); j++){
-										denominator = j;
-										avgrunning_time += thrs.getObjectfromList(j) - thrs.getObjectfromList(j-1);
+
+									if (thrs.sizeofList() == 1) {
+										avgrunning_time = thrs
+												.getObjectfromList(0);
+									} else {
+										for (int j = 1; j != thrs.sizeofList(); j++) {
+											denominator = j;
+											avgrunning_time += thrs
+													.getObjectfromList(j)
+													- thrs.getObjectfromList(j - 1);
 										}
 									}
-									
-									avgrunning_time = avgrunning_time/denominator;
-									if (avgrunning_time > cpulimit && thrs.availablePermits() != 0) {
-										thrs.aquirePermit();
-									} else if (thrs.availablePermits() == 0) {
+
+									avgrunning_time = avgrunning_time
+											/ denominator;
+									sumavgrunning_time += avgrunning_time;
+									if (getProfilingMode() == false) {
+										if (avgrunning_time > cpulimit
+												&& thrs.availablePermits() != 0) {
+											thrs.aquirePermit();
+										} else if (thrs.availablePermits() == 0) {
+											thrs.releasePermit();
+										}
+									}
+								}
+							}
+							if (getProfilingMode() == false) {
+								if (getLimitMode() == false) {
+									if (sumavgrunning_time > cpulimit) {
+										for (int j = 0; j < list.size(); j++) {
+											ThreadSemaphore thrs = list.get(j);
+											if (thrs.availablePermits() != 0) {
+												thrs.aquirePermit();
+											}
+										}
+									} else {
+										for (int k = 0; k < list.size(); k++) {
+											ThreadSemaphore thrs = list.get(k);
+											if (thrs.availablePermits() == 0) {
+												thrs.releasePermit();
+											}
+										}
+									}
+								}
+							}
+							if (getProfilingMode() == true) {
+								for (int l = 0; l < list.size(); l++) {
+									ThreadSemaphore thrs = list.get(l);
+									if (thrs.availablePermits() == 0) {
 										thrs.releasePermit();
 									}
 								}
@@ -196,9 +236,9 @@ public class CoreService extends Service implements PluginListener {
 				}
 			}).start();
 
-		}		
+		}
 	}
-	
+
 	public void registerPluginDetailsListener(PluginListener listener) {
 		pluginListeners.add(listener);
 	}
@@ -257,32 +297,37 @@ public class CoreService extends Service implements PluginListener {
 
 	/**
 	 * Adds a module to the core. Module will be part of the running system.
-	 * @param moduleDescriptor The descriptor of the module
+	 * 
+	 * @param moduleDescriptor
+	 *            The descriptor of the module
 	 * @return True if module was started successfully, false otherwise.
 	 */
 	public boolean installModule(ModuleDescriptor moduleDescriptor) {
-		return moduleManager.installModule(moduleDescriptor, pluginCollection, this);
+		return moduleManager.installModule(moduleDescriptor, pluginCollection,
+				this);
 	}
-	
+
 	/**
 	 * Remove a module from the core, module will stop running.
-	 * @param moduleId The id of the module
+	 * 
+	 * @param moduleId
+	 *            The id of the module
 	 * @return True if module was successfully removed, false otherwise.
 	 */
 	public boolean removeModule(String moduleId) {
 		return moduleManager.removeModule(moduleId, pluginCollection);
 	}
-	
+
 	@Override
 	public void onDestroy() {
-		
+
 		Log.e(TAG, "Service destroyed");
 		super.onDestroy();
 	}
 
 	@Override
 	public boolean newPlugin(Plugin plugin) {
-		Log.i(TAG,"newPlugin: "+plugin.getName());
+		Log.i(TAG, "newPlugin: " + plugin.getName());
 		pluginCollection.newPlugin(plugin);
 		for (PluginListener listener : pluginListeners) {
 			listener.newPlugin(plugin);
@@ -290,46 +335,50 @@ public class CoreService extends Service implements PluginListener {
 		return true;
 	}
 
-
 	public List<Plugin> getPlugins() {
 		return pluginCollection.getAllPlugins();
 	}
-	
+
 	public List<PluginDescriptor> getAvailablePlugins() {
 		return availablePlugins;
 	}
-	
-	public void registeringGCM(){
-        GCMRegistrar.checkDevice(this);
-        GCMRegistrar.checkManifest(this);
 
-        registration_ID = GCMRegistrar.getRegistrationId(this);
- 
-        if (registration_ID.equals("")) {
-            Log.i("GCM registration","Registration is not present, register now with GCM!");          
-            GCMRegistrar.register(this, SENDER_ID);
-        } else {
-        	Log.i("GCM registration","Device is already registered on GCM: " +registration_ID);
-            if (!GCMRegistrar.isRegisteredOnServer(this)) {
-            	registeringGCMonServer();
-            }
-        }
+	public void registeringGCM() {
+		GCMRegistrar.checkDevice(this);
+		GCMRegistrar.checkManifest(this);
+
+		registration_ID = GCMRegistrar.getRegistrationId(this);
+
+		if (registration_ID.equals("")) {
+			Log.i("GCM registration",
+					"Registration is not present, register now with GCM!");
+			GCMRegistrar.register(this, SENDER_ID);
+		} else {
+			Log.i("GCM registration", "Device is already registered on GCM: "
+					+ registration_ID);
+			if (!GCMRegistrar.isRegisteredOnServer(this)) {
+				registeringGCMonServer();
+			}
+		}
 	}
-	
-	public void registeringGCMonServer(){
-		TelephonyManager mngr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE); 
-        String imei=mngr.getDeviceId(); 
-        String sdk_version=String.valueOf(android.os.Build.VERSION.SDK_INT);
+
+	public void registeringGCMonServer() {
+		TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		String imei = mngr.getDeviceId();
+		String sdk_version = String.valueOf(android.os.Build.VERSION.SDK_INT);
 		PackageManager pm = this.getPackageManager();
-		
-		boolean cellular = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+
+		boolean cellular = pm
+				.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
 		boolean wifi = pm.hasSystemFeature(PackageManager.FEATURE_WIFI);
-		boolean bluetooth = pm.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+		boolean bluetooth = pm
+				.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
 		boolean gps = pm.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
-    	
-        RegisterToServer regTask = new RegisterToServer(this,imei,registration_ID,sdk_version,cellular,wifi,bluetooth,gps);
-        Thread thread = new Thread(regTask, "RegisterToServer");
-        thread.start();
+
+		RegisterToServer regTask = new RegisterToServer(this, imei,
+				registration_ID, sdk_version, cellular, wifi, bluetooth, gps);
+		Thread thread = new Thread(regTask, "RegisterToServer");
+		thread.start();
 	}
 
 	public static float totalRunTime(int pid, int tid) {
@@ -343,7 +392,7 @@ public class CoreService extends Service implements PluginListener {
 
 		try {
 			threadfile = FileUtils.readFile(threadfilepath);
-			} catch (Exception e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -355,21 +404,34 @@ public class CoreService extends Service implements PluginListener {
 			ex.printStackTrace();
 		}
 
-		float utime = Float.parseFloat(threadsplitArray[13]);		
-		float stime = Float.parseFloat(threadsplitArray[14]);		
+		float utime = Float.parseFloat(threadsplitArray[13]);
+		float stime = Float.parseFloat(threadsplitArray[14]);
 		float total_time = utime + stime;
-		
 
 		return total_time;
 	}
-	
+
 	public float getCpuUserSettings() {
-        SharedPreferences sharedPrefs = PreferenceManager
-                .getDefaultSharedPreferences(this);
-        float value = sharedPrefs.getFloat("my_slider", 0.5f);
-        float cpulimit = value*100f;
-        return cpulimit;
-    }
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		float value = sharedPrefs.getFloat("my_slider", 0.5f);
+		float cpulimit = value * 100f;
+		return cpulimit;
+	}
+
+	public boolean getLimitMode() {
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		boolean value = sharedPrefs.getBoolean("limit_mode", true);
+		return value;
+	}
+
+	public boolean getProfilingMode() {
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		boolean value = sharedPrefs.getBoolean("profiling_mode", false);
+		return value;
+	}
 
 	public class RegisterToServer implements Runnable {
 
