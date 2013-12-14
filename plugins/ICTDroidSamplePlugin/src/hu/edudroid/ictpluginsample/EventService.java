909f1fcbfdfcd5e52a86735255a21fca6f7d9cec
kissgabor <nanashiy@gmail.com>
2013-12-14 13:58:45 +0100

    Fixed and extended SamplePlugin with sending choosed limit information

--- plugins/ICTDroidSamplePlugin/src/hu/edudroid/ictpluginsample/EventService.java
+++ plugins/ICTDroidSamplePlugin/src/hu/edudroid/ictpluginsample/EventService.java
@@ -1,17 +1,24 @@
 package hu.edudroid.ictpluginsample;
 
+import hu.edudroid.interfaces.Constants;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class EventService extends Service {
 	private static final String TAG = EventService.class.getName();
 
-	public static final String PLUGIN_NAME="WiFi Plugin";
-	public static final String PING_METHOD_NAME="ping";
-	public static final String VERSION_CODE="v1.0";
+	public static final String PLUGIN_NAME="Sample Plugin";
+	
 	
 	private boolean started = false;
 	
@@ -32,21 +39,40 @@ public class EventService extends Service {
 	@Override
     public void onStart(Intent intent, int startId) {
 		if (!started) {
-			SamplePlugin samplePlugin = SamplePlugin.getInstance();
+			Log.i(TAG, "Starting EventService!");
+			
 			started = true;
-			Log.d(TAG, "Plugin event fired.");
+			SharedPreferences sharedPrefs = PreferenceManager
+					.getDefaultSharedPreferences(this);
+			Map<String,?> limits = sharedPrefs.getAll();
+			Set<String> methods = limits.keySet();
+			for (Iterator<String> i = methods.iterator(); i.hasNext();){
+				String method = (String) i.next();
+				String limit = (String) limits.get(method);
+				Intent myintent = new Intent();
+				myintent = new Intent(Constants.INTENT_ACTION_PLUGIN_LIMITS);
+				myintent.putExtra(Constants.INTENT_EXTRA_KEY_METHOD_NAME,method);
+				myintent.putExtra(Constants.INTENT_EXTRA_KEY_METHOD_LIMIT,limit);
+				this.sendBroadcast(myintent);
+			}
+			/*
+			SamplePlugin samplePlugin = SamplePlugin.getInstance();
+			List<String> result;
+			result = new ArrayList<String>();
+			result.add("something");
 			while(true) {
 				try {
+					
 					Thread.sleep((long)(Math.random() * 1000));
-					samplePlugin.event(SamplePlugin.FIRST_SAMPLE_EVENT_NAME, null, this);
+					samplePlugin.event(SamplePlugin.FIRST_SAMPLE_EVENT_NAME, result, this);
 					Log.d(TAG, "First event fired.");
 					Thread.sleep((long)(Math.random() * 1000));
-					samplePlugin.event(SamplePlugin.SECOND_SAMPLE_EVENT_NAME, null, this);
+					samplePlugin.event(SamplePlugin.SECOND_SAMPLE_EVENT_NAME, result, this);
 					Log.d(TAG, "Second event fired.");
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			}
+			}*/
 		}
 	}
 
--- plugins/ICTDroidSamplePlugin/src/hu/edudroid/ictpluginsample/MainActivity.java
+++ plugins/ICTDroidSamplePlugin/src/hu/edudroid/ictpluginsample/MainActivity.java
@@ -1,21 +1,39 @@
 package hu.edudroid.ictpluginsample;
 
+import hu.edudroid.interfaces.Constants;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
 	private static final int RESULT_SETTINGS = 1;
+	TextView tv1;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tv1 = (TextView) findViewById(R.id.textView1);
+		getLimit();
 	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		getLimit();
+	}	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +51,35 @@ public class MainActivity extends Activity {
 			break;
 		}
 		return true;
+	}
+	
+	public void getLimit(){
+		StringBuilder sb = new StringBuilder();
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		Map<String,?> limits = sharedPrefs.getAll();
+		Set<String> methods = limits.keySet();
+		for (Iterator<String> i = methods.iterator(); i.hasNext();){
+			String method = (String) i.next();
+			String limit = (String) limits.get(method);
+			Intent intent = new Intent();
+			intent = new Intent(Constants.INTENT_ACTION_PLUGIN_LIMITS);
+			intent.putExtra(Constants.INTENT_EXTRA_KEY_METHOD_NAME,method);
+			intent.putExtra(Constants.INTENT_EXTRA_KEY_METHOD_LIMIT,limit);
+			this.sendBroadcast(intent);
+			sb.append(method + ": ");
+			if (limit.equals("5"))
+			sb.append("Strict Limit\n");
+			else if (limit.equals("50"))
+				sb.append("Medium Limit\n");
+			else if (limit.equals("200"))
+				sb.append("Light Limit\n");
+			else if (limit.equals("0"))
+					sb.append("Limitless\n");
+			else{
+					sb.append(limit + "\n");
+				}
+		}
+		tv1.setText(sb.toString());
 	}
 }
