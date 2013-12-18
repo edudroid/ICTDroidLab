8d4b41384354f7e5df0d9f383c30ca4b12f7975d
Lajtha András Balázs <lajthabalazs@yahoo.com>
2013-12-18 20:50:10 +0100

    Added battery plugin.

---
+++ plugins/ICTDroidBatteryPlugin/src/hu/edudroid/ict_plugin_battery/BatteryPlugin.java
@@ -0,0 +1,168 @@
+package hu.edudroid.ict_plugin_battery;
+
+import hu.edudroid.ictplugin.PluginCommunicationInterface;
+import hu.edudroid.interfaces.AsyncMethodException;
+import hu.edudroid.interfaces.Quota;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+
+public class BatteryPlugin extends PluginCommunicationInterface {
+
+	private static BatteryPlugin instance;
+
+	public static BatteryPlugin getInstance() {
+		if (instance == null) {
+			instance = new BatteryPlugin();
+		}
+		return instance;
+	}
+	
+	private static final String PLUGIN_NAME = "Battery plugin";
+	private static final String PLUGIN_DESCRIPTION = "This plugin let's researchers access the battery status of the device.";
+
+	private static final String GET_BATTERY_STATUS = "Get battery status";
+
+	protected static final String BATTERY_LEVEL_CHANGED = "Battery level changed";
+	protected static final String CHARGING_STATE_CHANGED = "Charging state changed";
+
+	protected static final String BATTERY_LEVEL = "Battery level";
+	protected static final String IS_CHARGING = "Charging state";
+	protected static final String CHARGER_TYPE = "Charger type";
+	protected static final String CHARGER_TYPE_USB = "USB";
+	protected static final String CHARGER_TYPE_AC = "AC";
+
+	private static final String VERSION_CODE = "v1.0";
+
+	private static final List<String> methods;
+	private static final List<String> events;
+	private static final String PLUGIN_AUTHOR = "Lajtha Balázs";
+	@SuppressWarnings("unused")
+	private static final String TAG = BatteryPlugin.class.getName();
+
+	static {
+		List<String> tmpMethods = new ArrayList<String>();
+		List<String> tmpEvents = new ArrayList<String>();
+
+		tmpMethods.add(GET_BATTERY_STATUS);
+		tmpEvents.add(BATTERY_LEVEL_CHANGED);
+		tmpEvents.add(CHARGING_STATE_CHANGED);
+
+		methods = Collections.unmodifiableList(tmpMethods);
+		events = Collections.unmodifiableList(tmpEvents);
+	}
+
+	@Override
+	public String getVersionCode() {
+		return VERSION_CODE;
+	}
+
+	@Override
+	public String getName() {
+		return PLUGIN_NAME;
+	}
+	
+	@Override
+	public String getPackageName() {
+		return BatteryPlugin.class.getPackage().getName();
+	}
+
+	
+	@Override
+	public String getReceiverClassName() {
+		return BatteryPlugin.class.getName();
+	}
+
+	@Override
+	public List<String> getMethodNames() {
+		return methods;
+	}
+
+	@Override
+	public String getDescription() {
+		return PLUGIN_DESCRIPTION;
+	}
+
+	@Override
+	public String getAuthor() {
+		return PLUGIN_AUTHOR;
+	}
+
+	@Override
+	public List<String> getAllEvents() {
+		return events;
+	}
+
+	@Override
+	public Map<String, Object> callMethodSync(long callId, String method,
+			Map<String, Object> parameters, Object context)
+			throws AsyncMethodException {
+		return callMethodSync(callId, method, parameters, 0, context);
+	}
+
+	@Override
+	public Map<String, Object> callMethodSync(long callId, String method,
+			Map<String, Object> parameters, int quotaQuantity, Object context)
+			throws AsyncMethodException {
+		if (method.equals(GET_BATTERY_STATUS)) {
+			// Requesting battery status
+			IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+			Intent batteryStatus = ((Context) context).registerReceiver(null, ifilter);
+			return processIntent(batteryStatus);
+		} else {
+			return null;
+		}
+	}
+
+	protected void event(String eventName, Map<String, Object> result, Context context) {
+		super.fireEvent(eventName, result, context);
+	}
+
+	@Override
+	public List<Quota> getQuotas() {
+		List<Quota> quotas = new ArrayList<Quota>();
+		quotas.add(new FirstSampleQuota());
+		quotas.add(new SecondSampleQuota());
+		return quotas;
+	}
+
+	@Override
+	public Quota getQuotaForMethod(String method) {
+		return null;
+	}
+
+	@Override
+	public boolean validateQuota(Quota quota) {
+		return true;
+	}
+
+	@Override
+	public void consumeQuota(int identifier, int quantity) {
+	}
+
+	public static Map<String, Object> processIntent(Intent intent) {
+		Map<String, Object> values = new HashMap<String, Object>();
+		int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		values.put(BatteryPlugin.BATTERY_LEVEL, level);
+		int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+		boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING
+				|| status == BatteryManager.BATTERY_STATUS_FULL;
+		values.put(BatteryPlugin.IS_CHARGING, isCharging);
+		int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+		if(chargePlug == BatteryManager.BATTERY_PLUGGED_USB) {
+			values.put(BatteryPlugin.CHARGER_TYPE, BatteryPlugin.CHARGER_TYPE_USB);
+		}
+		if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
+			values.put(BatteryPlugin.CHARGER_TYPE, BatteryPlugin.CHARGER_TYPE_AC);
+		}
+		return values;
+	}
+}
---
+++ plugins/ICTDroidBatteryPlugin/src/hu/edudroid/ict_plugin_battery/SecondSampleQuota.java
@@ -0,0 +1,21 @@
+package hu.edudroid.ict_plugin_battery;
+
+import hu.edudroid.interfaces.Quota;
+
+public class SecondSampleQuota implements Quota{
+
+	@Override
+	public int getQuotaIdentifier() {
+		return 2;
+	}
+
+	@Override
+	public String getQuotaMeasurement() {
+		return "Second sample quota measurement";
+	}
+
+	@Override
+	public int[] getQuotaLevels() {
+		return new int[] {10, 200, 5000};
+	}
+}
