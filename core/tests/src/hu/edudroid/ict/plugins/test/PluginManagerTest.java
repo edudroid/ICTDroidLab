f2a78b384a8bc2051c62ce1f7b427cf190cbbe70
Lajtha András Balázs <lajthabalazs@yahoo.com>
2014-03-19 10:50:48 +0100

    Finished plugin manager tests.

--- core/tests/src/hu/edudroid/ict/plugins/test/PluginManagerTest.java
+++ core/tests/src/hu/edudroid/ict/plugins/test/PluginManagerTest.java
@@ -35,6 +35,73 @@ public class PluginManagerTest extends TestCase {
 		assertEquals(5, result.size());
 	}
 
+	/**
+	 * Tests if manager is capable of merging overlapping lists
+	 */
+	public void testMerge() {
+		List<PluginDescriptor> availablePlugins = new ArrayList<PluginDescriptor>();
+		PluginDescriptor firstDescriptor = new PluginDescriptor("First Plugin", "hu.edudroid.first", "Some description");
+		PluginDescriptor secondDescriptor = new PluginDescriptor("Second Plugin", "hu.edudroid.second", "Some description");
+		PluginDescriptor thirdDescriptor = new PluginDescriptor("Third Plugin", "hu.edudroid.third", "Some description");
+		PluginDescriptor fourthDescriptor = new PluginDescriptor("Fourth Plugin", "hu.edudroid.fourth", "Some description");
+		PluginDescriptor fifthDescriptor = new PluginDescriptor("Fifth Plugin", "hu.edudroid.fifth", "Some description");
+		availablePlugins.add(firstDescriptor);
+		availablePlugins.add(secondDescriptor);
+		availablePlugins.add(thirdDescriptor);
+		availablePlugins.add(fourthDescriptor);
+		availablePlugins.add(fifthDescriptor);
+		List<Plugin> downloadedPlugins = new ArrayList<Plugin>();
+		Plugin fourthPlugin = new MockPlugin("Fourth Plugin", "hu.edudroid.fourth");
+		Plugin fifthPlugin = new MockPlugin("Fifth Plugin", "hu.edudroid.fifth");
+		downloadedPlugins.add(fourthPlugin);
+		downloadedPlugins.add(fifthPlugin);
+		List<PluginDescriptor> result = PluginManager.getAvailablePlugins(availablePlugins, downloadedPlugins);
+		assertEquals(5, result.size());
+	}
+
+	/**
+	 * Tests if manager is capable of merging a null descriptor list
+	 */
+	public void testNoDescriptors() {
+		List<PluginDescriptor> availablePlugins = null;
+		List<Plugin> downloadedPlugins = new ArrayList<Plugin>();
+		Plugin fourthPlugin = new MockPlugin("Fourth Plugin", "hu.edudroid.fourth");
+		Plugin fifthPlugin = new MockPlugin("Fifth Plugin", "hu.edudroid.fifth");
+		downloadedPlugins.add(fourthPlugin);
+		downloadedPlugins.add(fifthPlugin);
+		List<PluginDescriptor> result = PluginManager.getAvailablePlugins(availablePlugins, downloadedPlugins);
+		assertEquals(2, result.size());
+	}
+	
+	/**
+	 * Tests if manager is capable of merging when there are no installed plugins
+	 */
+	public void testNoPlugins() {
+		List<PluginDescriptor> availablePlugins = new ArrayList<PluginDescriptor>();
+		PluginDescriptor firstDescriptor = new PluginDescriptor("First Plugin", "hu.edudroid.first", "Some description");
+		PluginDescriptor secondDescriptor = new PluginDescriptor("Second Plugin", "hu.edudroid.second", "Some description");
+		PluginDescriptor thirdDescriptor = new PluginDescriptor("Third Plugin", "hu.edudroid.third", "Some description");
+		PluginDescriptor fourthDescriptor = new PluginDescriptor("Fourth Plugin", "hu.edudroid.fourth", "Some description");
+		availablePlugins.add(firstDescriptor);
+		availablePlugins.add(secondDescriptor);
+		availablePlugins.add(thirdDescriptor);
+		availablePlugins.add(fourthDescriptor);
+		List<Plugin> downloadedPlugins = null;
+		List<PluginDescriptor> result = PluginManager.getAvailablePlugins(availablePlugins, downloadedPlugins);
+		assertEquals(4, result.size());
+	}
+	
+	/**
+	 * Tests if manager is capable of merging when there are no installed plugins
+	 */
+	public void testNoData() {
+		List<PluginDescriptor> availablePlugins = null;
+		List<Plugin> downloadedPlugins = null;
+		List<PluginDescriptor> result = PluginManager.getAvailablePlugins(availablePlugins, downloadedPlugins);
+		assertEquals(0, result.size());
+	}
+
+
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
