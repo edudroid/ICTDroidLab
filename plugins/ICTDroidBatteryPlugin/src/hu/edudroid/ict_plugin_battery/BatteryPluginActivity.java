bd42af1069a1cb0d41f32afc18bf395916a58551
Lajtha András Balázs <lajthabalazs@yahoo.com>
2014-04-04 12:40:04 +0200

    Added void layout to battery plugin.

--- plugins/ICTDroidBatteryPlugin/src/hu/edudroid/ict_plugin_battery/BatteryPluginActivity.java
+++ plugins/ICTDroidBatteryPlugin/src/hu/edudroid/ict_plugin_battery/BatteryPluginActivity.java
@@ -7,5 +7,6 @@ public class BatteryPluginActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 	}
 }
