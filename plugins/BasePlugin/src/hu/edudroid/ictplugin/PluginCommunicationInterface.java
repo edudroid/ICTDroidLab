113f9497729304fb22ad8890cbd6dd1c3b131bfe
Lajtha Balazs <lajthabalazs@yahoo.com>
2014-11-28 14:36:13 +0100

    Added return statement to communication interface receive method.

--- plugins/BasePlugin/src/hu/edudroid/ictplugin/PluginCommunicationInterface.java
+++ plugins/BasePlugin/src/hu/edudroid/ictplugin/PluginCommunicationInterface.java
@@ -30,7 +30,13 @@ public class PluginCommunicationInterface {
 		this.plugin = plugin;
 	}
 	
-	public void onReceive(Context context, Intent intent) {
+	/**
+	 * Processes an intent. If the intent is a framework intent, returns true.
+	 * @param context The context to be used to send broadcasts
+	 * @param intent The intent received
+	 * @return True if the intent was processed (even if with an error) or false if the intent was not from the framework
+	 */
+	public boolean onReceive(Context context, Intent intent) {
 		Log.d(TAG, "Intent received " + intent.getAction());
 		if (intent.getAction().equals(Constants.INTENT_ACTION_PLUGIN_QUOTAS)) {
 			Intent response = new Intent(Constants.INTENT_ACTION_QUOTA_DESCRIPTION);
@@ -45,6 +51,7 @@ public class PluginCommunicationInterface {
 			}
 			response.putExtra(Constants.INTENT_EXTRA_KEY_QUOTAS, quotaDescr);			
 			context.sendBroadcast(response);
+			return true;
 		}
 		
 		if (intent.getAction().equals(Constants.INTENT_ACTION_PLUGIN_POLL)) {
@@ -63,6 +70,7 @@ public class PluginCommunicationInterface {
 			response.putStringArrayListExtra(Constants.INTENT_EXTRA_KEY_PLUGIN_EVENTS,
 					new ArrayList<String>(plugin.getAllEvents()));
 			context.sendBroadcast(response);
+			return true;
 		} 
 		
 		if (intent.getAction().equals(Constants.INTENT_ACTION_CALL_METHOD)) {
@@ -76,20 +84,25 @@ public class PluginCommunicationInterface {
 				try {
 					PluginResult result = plugin.callMethodSync(callId, methodName, params, quotaLimits, context);
 					reportResult(callId, Constants.INTENT_EXTRA_VALUE_RESULT, methodName, result, context);
+					return true;
 				} 
 				catch (AsyncMethodException a){
 					Log.i(TAG,"This is an async method.");
+					return true;
 				}
 				catch (Exception e) {
 					Map<String,Object> error = new HashMap<String, Object>();
 					error.put(Constants.ERROR_MESSAGE_KEY, e.getMessage());
 					reportResult(callId, Constants.INTENT_EXTRA_VALUE_ERROR, methodName, new PluginResult(error, null), context);
+					return true;
 				}
 			} catch (IOException e1) {
 				e1.printStackTrace();
 				Log.e(TAG, "Error parsing params.", e1);
+				return true;
 			}
 		}
+		return false;
 	}
 	
 	public void reportResult(long callId, String resultCode, String method, PluginResult results, Context context) {
