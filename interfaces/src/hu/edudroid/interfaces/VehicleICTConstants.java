ea5d91efbfd8351ef636b238ffc8f84c4586b370
Miklós Máté <mtmkls@gmail.com>
2014-12-13 14:25:46 +0100

    Add VehicleICTModule

--- interfaces/src/hu/edudroid/interfaces/VehicleICTConstants.java
+++ interfaces/src/hu/edudroid/interfaces/VehicleICTConstants.java
@@ -3,6 +3,6 @@ package hu.edudroid.interfaces;
 public class VehicleICTConstants {
 	public static final String PLUGIN_NAME = "Vehicle ICT plugin";
 	public static final String METHOD_SUBSCRIBE = "Subscribe";
-	public static final Object METHOD_UNSUBSCRIBE = "Unsubscribe";
+	public static final String METHOD_UNSUBSCRIBE = "Unsubscribe";
 	public static final String EVENT_VEHICLE_DATA_RECEIVED = "Vehicle data received";
 }
