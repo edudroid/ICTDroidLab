8211a649409850777d1ea3842b776c7c7950e960
Szbuli <szbuli@hotmail.com>
2015-03-14 14:15:46 +0100

    ModuleWrapper changed to proxy TimeService, Logger and PluginCollection

--- core/src/hu/edudroid/module/ModuleWrapper.java
+++ core/src/hu/edudroid/module/ModuleWrapper.java
@@ -3,8 +3,10 @@ package hu.edudroid.module;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
@@ -15,6 +17,8 @@ import android.util.Log;
 import hu.edudroid.ict.ModuleStatsListener;
 import hu.edudroid.interfaces.Logger;
 import hu.edudroid.interfaces.Module;
+import hu.edudroid.interfaces.ModuleTimerListener;
+import hu.edudroid.interfaces.Plugin;
 import hu.edudroid.interfaces.PluginCollection;
 import hu.edudroid.interfaces.Preferences;
 import hu.edudroid.interfaces.TimeServiceInterface;
@@ -25,24 +29,31 @@ import hu.edudroid.interfaces.TimeServiceInterface;
  * @author lajthabalazs
  *
  */
-public class ModuleWrapper extends Module {
+public class ModuleWrapper extends Module implements TimeServiceInterface, Logger, PluginCollection {
 	
 	private static final String TAG = ModuleWrapper.class.getName();
 	private static final String SHARED_PREF_PREFIX = "STATS_";
 	private final Module module;
 	private HashSet<ModuleStatsListener> moduleStatsListeners = new HashSet<ModuleStatsListener>();
 	private SharedPreferences statPrefs;
-	private ModuleDescriptor descriptor;
+	private ModuleDescriptor descriptor;	
+	private TimeServiceInterface wrapperTimeService;	
+	private Logger wrapperLogger;						
+	private PluginCollection wrapperPluginCollection;	
 	
 	public ModuleWrapper(ModuleDescriptor descriptor, Constructor<Module> constructor, Preferences prefs, Logger logger, PluginCollection pluginCollection, TimeServiceInterface timeService, Context context) throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
 		super(prefs, logger, pluginCollection, timeService);
-		Log.i(TAG,"Calling module constructor");
-		module = constructor.newInstance(prefs, logger, pluginCollection, timeService);
+		Log.i(TAG,"Calling module constructor");		
+		this.wrapperTimeService = timeService; 				
+		this.wrapperLogger = logger;						
+		this.wrapperPluginCollection = pluginCollection;		
+		module = constructor.newInstance(prefs, this, this, this);
 		statPrefs = context.getSharedPreferences(SHARED_PREF_PREFIX + descriptor.moduleId, Context.MODE_PRIVATE);
-		this.descriptor = descriptor;
+		this.descriptor = descriptor;			
 	}
 
 	public Map<String, String> getStats() {
+		Log.d(TAG, "ModuleWrapper getStats");
 		int timerEventCount = statPrefs.getInt(ModuleStatsListener.STAT_KEY_TIMERS_FIRED, 0);
 		long lastTimerEvent = statPrefs.getLong(ModuleStatsListener.STAT_KEY_LAST_TIMER_EVENT, 0);
 		
@@ -53,17 +64,17 @@ public class ModuleWrapper extends Module {
 	}
 
 	private void statsChangted() {
-		Log.e(TAG, "Stats changed");
+		Log.e(TAG, "ModuleWrapper stats changed");
 		Map<String, String> unmodifiableStats = Collections.unmodifiableMap(getStats());
 		for (ModuleStatsListener listener : moduleStatsListeners) {
 			listener.moduleStatsChanged(descriptor.moduleId, unmodifiableStats);
 		}
 	}
 
-
 	@Override
 	public void onResult(long id, String plugin, String pluginVersion,
 			String methodName, Map<String, Object> result) {
+		Log.d(TAG, "ModuleWrapper onResult");
 		module.onResult(id, plugin, pluginVersion, methodName, result);
 	}
 
@@ -76,6 +87,7 @@ public class ModuleWrapper extends Module {
 	@Override
 	public void onEvent(String plugin, String version, String eventName,
 			Map<String, Object> extras) {
+		Log.d(TAG, "ModuleWrapper onEvent");
 		Editor editor = statPrefs.edit();
 		editor.putLong(ModuleStatsListener.STAT_KEY_LAST_TIMER_EVENT, System.currentTimeMillis());
 		editor.commit();
@@ -85,6 +97,7 @@ public class ModuleWrapper extends Module {
 
 	@Override
 	public void onTimerEvent() {
+		Log.d(TAG, "ModuleWrapper onTimerEvent");
 		int timerEventCount = statPrefs.getInt(ModuleStatsListener.STAT_KEY_TIMERS_FIRED, 0);
 		timerEventCount ++;
 		Editor editor = statPrefs.edit();
@@ -102,19 +115,85 @@ public class ModuleWrapper extends Module {
 	
 	@Override
 	public void init() {
+		Log.d(TAG, "ModuleWrapper init");
 		module.init();
 	}
 	
 	public void registerModuleStatsListener(ModuleStatsListener listener) {
+		Log.d(TAG, "ModuleWrapper registerModuleStatsListener");
 		moduleStatsListeners.add(listener);
 	}
 
 	public void unregisterModuleStatsListenerListener(ModuleStatsListener listener) {
+		Log.d(TAG, "ModuleWrapper unregisterModuleStatsListenerListener");
 		moduleStatsListeners.remove(listener);
 	}
 
 	public ModuleDescriptor getDescriptor() {
+		Log.d(TAG, "ModuleWrapper getDescriptor");
 		return descriptor;
+	}
+
+	@Override
+	public void cancelAll() {
+		this.wrapperTimeService.cancelAll();		
+	}
+
+	@Override
+	public void runAt(int delay, ModuleTimerListener listener) {
+		if (this.module == listener) {
+			Log.e(TAG, "ModuleWrapper called from foreign module");
+		}
+		this.wrapperTimeService.runAt(delay, this);	
+	}
+
+	@Override
+	public void runAt(Date when, ModuleTimerListener listener) {
+		if (this.module == listener) {
+			Log.e(TAG, "ModuleWrapper called from foreign module");
+		}
+		this.wrapperTimeService.runAt(when, this);
+	}
+
+	@Override
+	public void runPeriodic(int delay, int periodicity, int tickCount, ModuleTimerListener listener) {
+		if (this.module == listener) {
+			Log.e(TAG, "ModuleWrapper called from foreign module");
+		}
+		this.wrapperTimeService.runPeriodic(delay, periodicity, tickCount, this);
+	}
+
+	@Override
+	public void runPeriodic(Date when, int periodicity, int tickCount, ModuleTimerListener listener) {
+		if (this.module == listener) {
+			Log.e(TAG, "ModuleWrapper called from foreign module");
+		}
+		this.wrapperTimeService.runPeriodic(when, periodicity, tickCount, this);
+	}
+
+	@Override
+	public Plugin getPluginByName(String string) {
+		return this.wrapperPluginCollection.getPluginByName(string);
+	}
+
+	@Override
+	public List<Plugin> getAllPlugins() {
+		return this.wrapperPluginCollection.getAllPlugins();
+	}
+
+	@Override
+	public void e(String tag, String message) {
+		this.wrapperLogger.e(tag, message);		
+	}
+
+	@Override
+	public void d(String tag, String message) {
+		this.wrapperLogger.d(tag, message);		
+	}
+
+	@Override
+	public void i(String tag, String message) {
+		this.wrapperLogger.i(tag, message);		
 	}
 
 
